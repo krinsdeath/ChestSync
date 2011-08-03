de1ffbcd11f305e2d844fda79af219f5634281a7
Jeff Wardian <krinsdeath@hotmail.com>
2011-08-03 15:00:45 -0700

    utility class; PlayerListener class;
    ChestListener

---
+++ src/main/java/net/krinsoft/chestsync/ChestListener.java
@@ -0,0 +1,21 @@
+package net.krinsoft.chestsync;
+
+import org.getspout.spoutapi.event.inventory.InventoryCloseEvent;
+import org.getspout.spoutapi.event.inventory.InventoryOpenEvent;
+
+/**
+ *
+ * @author krinsdeath
+ */
+
+class ChestListener {
+
+	public void onInventoryOpen(InventoryOpenEvent event) {
+
+	}
+
+	public void onInventoryClose(InventoryCloseEvent event) {
+		
+	}
+
+}
---
+++ src/main/java/net/krinsoft/chestsync/PlayerListener.java
@@ -0,0 +1,44 @@
+package net.krinsoft.chestsync;
+
+import org.bukkit.Location;
+import org.bukkit.entity.Player;
+import org.bukkit.event.block.Action;
+import org.bukkit.event.player.PlayerInteractEvent;
+import org.getspout.spoutapi.block.SpoutChest;
+import org.getspout.spoutapi.player.SpoutPlayer;
+
+/**
+ *
+ * @author krinsdeath
+ */
+
+class PlayerListener extends org.bukkit.event.player.PlayerListener {
+	private ChestSync plugin;
+
+	public PlayerListener(ChestSync aThis) {
+		plugin = aThis;
+	}
+
+	@Override
+	public void onPlayerInteract(PlayerInteractEvent event) {
+		if (event.isCancelled()) { return; }
+		if (event.getClickedBlock() == null) { return; }
+
+		if (event.getAction() == Action.LEFT_CLICK_AIR || event.getAction() == Action.LEFT_CLICK_BLOCK) { return; }
+
+		if (event.getClickedBlock().getState() instanceof SpoutChest) {
+			Location l = event.getClickedBlock().getLocation();
+			SyncedChest c = SyncedChest.getSyncedChest(l);
+			if (c != null) {
+				if (!Utility.checkPermission(event.getPlayer(), "access", c.getNetwork())) {
+					Utility.error(event.getPlayer(), "permission");
+					event.setCancelled(true);
+					return;
+				}
+				((SpoutPlayer)event.getPlayer()).openInventoryWindow(SyncedChest.getInventory(c.getNetwork()), c.getLocation());
+				event.setCancelled(true);
+			}
+		}
+	}
+
+}
